1b4f0f22b1304f6ff38010337c6062a1d02b8d42
Manishi <manishirani30501@gmail.com>
2023-11-03 19:40:26 +0530

    final commit

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const express = require ('express')
 const workoutRoutes =require('./routes/workouts')
 const mongoose = require('mongoose')
-
+const path = require("path")
 const app = express()
 
 //middleware which will give me the routes
@@ -15,6 +15,13 @@ app.use((req,res,next) => {
 
 // routes
 app.use('/api/workouts',workoutRoutes)
+
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+app.get("*", (req, res) =>
+    // res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+    res.sendFile(path.join(__dirname, "../frontend/build/index.html"))
+);
 
 //connecting to the database
 mongoose.connect(process.env.MONGO_URI)
